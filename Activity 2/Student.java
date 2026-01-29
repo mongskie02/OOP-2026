db52b3a3dd3c87e2543ec2c70a290fa551a464e3
mongskie02 <raymartjustine.angelo@lorma.edu>
2026-01-29 20:49:11 +0800

    Update Student.java

--- Activity 2/Student.java
+++ Activity 2/Student.java
@@ -1,28 +1,44 @@
 public class Student {
+    int studentID;
+    String firstName;
+    String middleName;
+    String lastName;
+    String gender;
+    String email;
+    static int totalStudents;
 
-    private int id;
-    private String firstName, middleName, lastName, gender, email;
-    private static int totalStudents = 0;
-
-    public Student(int id, String firstName, String middleName,
-                   String lastName, String gender, String email) {
-        this.id = id;
+    public Student(int studentID, String firstName, String middleName, String lastName, String gender, String email) {
+        this.studentID = studentID;
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
         this.gender = gender;
         this.email = email;
-        totalStudents++;
     }
 
+    public Student () {
+        
+    }
+    
+    public void displayStudentInfo() {
+        System.out.println("----STUDENT INFORMATION----");
+        System.out.println("STUDENT ID: " + this.studentID);
+        System.out.println("FIRST NAME:" + this.middleName);
+        System.out.println("MIDDLE NAME: " + this.middleName );
+        System.out.println("LAST NAME: " + this.lastName);
+        System.out.println("GENDER: " + this.gender);
+        System.out.println("EMAIL: " + this.email);
+
+    }
     public static int getTotalStudents() {
         return totalStudents;
+
+
     }
 
-    public void displayStudentInfo() {
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + firstName + " " + middleName + " " + lastName);
-        System.out.println("Gender: " + gender);
-        System.out.println("Email: " + email);
-    }
+
+    
+
+
+
 }
